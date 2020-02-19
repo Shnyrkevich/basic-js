68a200e412ab49c8f68eee97e88994e3306e9795
Евгений Шныркевич <mogrein.dark@gmail.com>
2020-02-19 20:11:45 +0300

    feat: realize count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,15 @@
-module.exports = function countCats(/* matrix */) {
+module.exports = function countCats(mas) {
   throw 'Not implemented';
-  // remove line with error and write your code here
+  
+  let counter = 0;
+
+  for(let i = 0; i < mas.length; i++){
+    for(let j = 0; j < mas[i].length; j++){
+      if(mas[i][j] == "^^"){
+        counter++;
+      }
+    }
+  }
+  return counter;
+
 };

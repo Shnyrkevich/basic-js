12713b2a58e2faabcd0995fef9bce8c231205030
Евгений Шныркевич <mogrein.dark@gmail.com>
2020-02-20 17:30:37 +0300

    feat: realize dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,31 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
+module.exports = function createDreamTeam(members) {
+  //throw 'Not implemented';
+
+  let toLetters = [];
+  let counter = 0;
+  let name = "";
+
+  if(members )
+
+  for(let i = 0; i < members.length; i++){
+    if(typeof(members[i]) === 'string'){
+      members[i] = members[i].trim();
+      toLetters[i] = members[i][0].toUpperCase();
+      counter++;
+    }
+  }
+
+  toLetters.sort();
+
+  if(toLetters.length > counter){
+    toLetters = toLetters.splice(0, counter);
+  }
+
+  for(let i =0; i < toLetters.length; i++){
+    name += toLetters[i];
+  }
+
+  return name;
+
   // remove line with error and write your code here
 };

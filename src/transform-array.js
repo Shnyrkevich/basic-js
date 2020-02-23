7211c07e3a5190f38b4dc0ee4631a274ad9ef501
Евгений Шныркевич <mogrein.dark@gmail.com>
2020-02-23 18:08:31 +0300

    feat: realize transform arrays function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,46 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+    if(!Array.isArray(arr)){
+        throw new Error();
+    }
+    
+    if(arr === []){
+        return arr;
+    }
+
+    let mas = [];
+
+    for(let i = 0; i <= arr.length-1; i++){
+        switch(arr[i]){
+            case '--discard-next':
+                if(i != arr.length-1){
+                    i++;
+                    //console.log(i, mas);
+                }
+                break;
+            case '--discard-prev':
+                if( i != 0){
+                    mas.pop();
+                    //console.log(i, mas);
+                }
+                break;
+            case '--double-next':
+                if( i != arr.length-1){
+                    mas.push(arr[i+1]);
+                    //console.log(i, mas);
+                }
+                break;
+            case '--double-prev':
+                if( i != 0){
+                    mas.push(arr[i-1]);
+                    //console.log(i, mas);
+                }
+                break;
+            default:
+                mas.push(arr[i]);
+                //console.log(i, mas);
+        }
+    }
+    return mas;
+
 };

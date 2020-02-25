714eaa44f7be8b8037f684c74619df775a1ee826
Евгений Шныркевич <mogrein.dark@gmail.com>
2020-02-25 21:27:07 +0300

    fix: rework depth calculator and reinstall test

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,12 +1,12 @@
 module.exports = class DepthCalculator {
-   calculateDepth(arr, depth = 1) {
+    calculateDepth(arr, depth = 1) {
         //console.log("Array:", arr);
-        for(let i = 0; i <= arr.length-1; i++){
-            if(Array.isArray(arr[i])){
-                depth++;
-                depth = this.calculateDepth(arr[i], depth);
-                //console.log("Counter:", depth);
-               // break;
+        for (let i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) {
+                depth += this.calculateDepth([].concat(...arr), depth);//concat объеденяет массив например [1,2,3].concat[1,2] = [1,2,3,1,2]
+                //console.log("Counter:", depth);                        //spread(..,) разворачивает массив и передает его поэлементно 
+                break;                                                 //Решение подсмотрел у BlazarQSO
+                                                                       //Мое решение проверяло каждый элемент массива и подсчитывало общее количество массивов
             }
         }
         return depth;
